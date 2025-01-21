ea409a54a81478d0db36103d1720e33d11462370
Gabriel Siega <Gabriel.siega@yahoo.com>
2025-01-21 10:00:06 +0800

    Changed Search bar name and login routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import App from './App.jsx';
+import LoginPage from './Login.jsx'; // Make sure to create this component
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        {/* Redirect the root path to /login */}
+        <Route path="/" element={<Navigate to="/Login" />} />
+        {/* Define the /login route */}
+        <Route path="/Login" element={<LoginPage />} />
+        {/* You can add other routes as needed
+        <Route path="/home" element={<App />} /> */}
+      </Routes>
+    </Router>
+  </StrictMode>
+);
